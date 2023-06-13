e21dc8c8ed45477417d2b8c52a4cab00b4c8d2a8
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-06-13 15:15:15 +0300

    Update README.md

--- src/item.py
+++ src/item.py
@@ -13,18 +13,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        Рассчитывает общую стоимость конкретного товара в мpoагазине.
 
         :return: Общая стоимость товара.
         """
-        pass
+        return float(self.price * self.quantity)
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = float(self.price * self.pay_rate)
+
+        Item.all.append(Item(self.name, self.price, self.quantity))
